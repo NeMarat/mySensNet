727985c61db9d4af2ec0fb31d9e72f5939d1b915
NeMarat <maratn@gmail.com>
2016-01-17 19:35:51 +0300

    rf433 moved from hall sensor to leDriver

--- hallSensor/hallSensor.ino
+++ hallSensor/hallSensor.ino
@@ -1,12 +1,10 @@
 #include <SPI.h>
 #include <MySensor.h>  
 #include <DHT.h>  
-#include <RCSwitch.h>
 
 #define CHILD_ID_HUM 0
 #define CHILD_ID_TEMP 1
 #define CHILD_ID_PIR 2
-#define CHILD_ID_433 3
 #define HUMIDITY_SENSOR_PIN 7
 #define PIR_SENSOR_PIN 6
 
@@ -22,16 +20,13 @@ boolean tripped;
 
 DHT dht;
 MySensor gw;
-RCSwitch mySwitch = RCSwitch();
 boolean metric = true;
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
 MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
 MyMessage msgPir(CHILD_ID_PIR, V_TRIPPED);
-MyMessage msg433(CHILD_ID_433, V_VAR1);
 
 void setup() { 
   pinMode(PIR_SENSOR_PIN, INPUT);
-  mySwitch.enableReceive(0);  // Receiver on inerrupt 0 => that is pin #2
   gw.begin();
   dht.setup(HUMIDITY_SENSOR_PIN);
 
@@ -40,7 +35,6 @@ void setup() {
   gw.present(CHILD_ID_HUM, S_HUM);
   gw.present(CHILD_ID_TEMP, S_TEMP);
   gw.present(CHILD_ID_PIR, S_MOTION);
-  gw.present(CHILD_ID_433, S_CUSTOM);
   lastSend=millis();
 }
 
@@ -52,12 +46,7 @@ void loop() {
   if (!tripped) {
     tripped = digitalRead(PIR_SENSOR_PIN) == HIGH;
   } 
-  
-  if (mySwitch.available()) {
-      gw.send(msg433.set(mySwitch.getReceivedValue()));
-      mySwitch.resetAvailable();
-    }
-  
+
   if (sendTime) {
     sendTime = false;
     lastSend=nowTime;
