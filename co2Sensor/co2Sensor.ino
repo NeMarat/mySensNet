d4d7c06754fb77ebeb8f49a37ec5764616bab56c
NeMarat <maratn@gmail.com>
2016-05-21 17:08:27 +0300

    CO2 sensor based on MH-Z19 added

---
+++ co2Sensor/co2Sensor.ino
@@ -0,0 +1,56 @@
+#include <MySensor.h>  
+
+#define CHILD_ID_CO 0
+
+byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79}; 
+unsigned char response[9];
+
+uint64_t SLEEP_TIME = 180000;
+uint64_t nowTime;
+uint64_t lastSend;
+uint32_t coPpm;
+uint32_t responseHigh;
+uint32_t responseLow;
+bool sendTime;
+uint8_t i; 
+int8_t crc = 0;
+
+MySensor gw;
+boolean metric = true;
+MyMessage msgCo(CHILD_ID_CO, V_LEVEL);
+
+void setup() { 
+  gw.begin();
+  Serial.begin(9600);
+  gw.sendSketchInfo("CO2 sensor", "1.1");
+  gw.present(CHILD_ID_CO, S_AIR_QUALITY);
+  lastSend=millis();
+}
+
+void loop() {
+  gw.process();
+  nowTime = millis();
+  sendTime = nowTime - lastSend > SLEEP_TIME;
+
+  if (sendTime) {
+    sendTime = false;
+    lastSend=nowTime;
+    memset(response, 0, 9);
+    Serial.write(cmd, 9);
+    Serial.readBytes(response, 9);
+    
+  for (i = 1; i < 8; i++) { crc+=response[i]; }
+  crc = 255 - crc;
+  crc++;
+
+  if ( !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc) ) {
+    coPpm=0;
+  } else {
+    responseHigh = (unsigned int) response[2];
+    responseLow = (unsigned int) response[3];
+    coPpm=(256*responseHigh) + responseLow;
+  }
+  gw.send(msgCo.set(coPpm));  
+  }
+}
+
