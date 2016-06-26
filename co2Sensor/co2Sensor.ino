8131313a323b8d00e0d1b921876ca4e3d7a827a4
NeMarat <maratn@gmail.com>
2016-06-26 11:44:38 +0300

    .

--- co2Sensor/co2Sensor.ino
+++ co2Sensor/co2Sensor.ino
@@ -1,22 +1,31 @@
 #include <SPI.h>
+#include <Wire.h> 
 #include <MySensor.h>  
+#include <LiquidCrystal_I2C.h>
 
 #define CHILD_ID_CO 0
+#define CHILD_ID_V1 1
+#define CHILD_ID_V2 2
+#define CHILD_ID_V3 3
 #define BATT_PIN    A1
 
 byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79}; 
 unsigned char response[9];
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-uint64_t SLEEP_TIME = 20000;
+uint64_t SLEEP_TIME = 20000; //measure every 20 sec
+uint16_t REWRITE_TIME = 6; // rewrite display ewery x measuments
 uint64_t nowTime;
 uint64_t lastSend;
 uint32_t coPpm;
 uint32_t lastPpm;
 uint32_t responseHigh;
 uint32_t responseLow;
-bool sendTime;
+bool sendTime, reWrite;
 uint8_t i; 
 byte crc = 0;
+String v1, v2, v3, r1, r2;
+byte s1, s2;
 
 MySensor gw;
 boolean metric = true;
@@ -28,12 +37,43 @@ float baTest() {
   return(bv);
 }
 
+void incomingMessage (const MyMessage &message) {
+  char *buf;
+  if (message.type==V_VAR1) {  
+    v1=message.getString(buf);
+  }
+  if (message.type==V_VAR2) {  
+    v2=message.getString(buf);
+  }
+  if (message.type==V_VAR3) {  
+    v3=message.getString(buf);
+  }
+  s1 = 16-length(v1)-length(v2)-1;
+  s2 = 16-length(v3)-8-1; //CO2=XXXX
+  if (s1 < 0){
+    s1=1;
+    v1=v1.substring(0, min(7, length(v1));
+    v2=v2.substring(0, min(8, length(v2));
+  }
+  if (s2 < 0){
+    s2=1;
+    v3=v3.substring(0, min(7, length(v3));
+  }
+}
+
 void setup() { 
-  gw.begin();
+  gw.begin(incomingMessage);
   Serial.begin(9600);
+  lcd.begin();
+  lcd.backlight();
+  lcd.noBlink();
+  lcd.noCursor(); 
   pinMode(BATT_PIN, INPUT);
   gw.sendSketchInfo("CO2 sensor", "1.2");
   gw.present(CHILD_ID_CO, S_AIR_QUALITY);
+  gw.present(CHILD_ID_V1, S_CUSTOM);
+  gw.present(CHILD_ID_V2, S_CUSTOM);
+  gw.present(CHILD_ID_V3, S_CUSTOM);
   lastSend=millis();
   lastPpm=0;
 }
@@ -67,5 +107,10 @@ void loop() {
     }
   }
   }
+  if (reWrite) {
+    r1=v1; r2=v3;
+    for (byte i=0; i < s1; i++) { r1 = r1+' '; }
+    for (byte i=0; i < s2; i++) { r2 = r2+' '; }
+  } 
 }
 
