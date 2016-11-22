b1e301696d320229cba57f637ee1892fc55a6ddb
NeMarat <maratn@gmail.com>
2016-11-22 22:49:27 +0300

    flashind add to ledriver for using with doorbell

--- leDriver/leDriver.ino
+++ leDriver/leDriver.ino
@@ -4,10 +4,14 @@
 
 #define CHILD_ID_LED 0
 #define CHILD_ID_433 1
+#define CHILD_ID_FLS 2
+#define CHILD_ID_DLY 3
 #define LED_PIN 5
 #define FADE_DELAY 10
 
 uint8_t led;
+uint8_t fls;
+uint16_t dly=50;
 
 MySensor gw;
 RCSwitch mySwitch = RCSwitch();
@@ -29,8 +33,20 @@ void fadeToLevel( int toLevel ) {
   }
 }
 
+void ledFlash () {
+  uint8_t leOld = led;
+  fadeToLevel(0);
+  while (fls > 0) {
+    fls--;
+    analogWrite(LED_PIN, 255);
+    delay(dly);
+    analogWrite(LED_PIN, 0);
+  }
+  fadeToLevel(leOld);
+}
+
 void incomingMessage(const MyMessage &message) {
-  if (message.type == V_LIGHT || message.type == V_DIMMER) {
+  if (message.sensor == CHILD_ID_LED && (message.type == V_LIGHT || message.type == V_DIMMER)) {
     
     //  Retrieve the power or dim level from the incoming request message
     int requestedLevel = atoi( message.data );
@@ -47,6 +63,12 @@ void incomingMessage(const MyMessage &message) {
     // Inform the gateway of the current DimmableLED's SwitchPower1 and LoadLevelStatus value...
     gw.send(lightMsg.set(led));
   }
+  if (message.sensor == CHILD_ID_FLS && message.type == V_LIGHT) {
+    fls=message.getByte();
+  }
+  if (message.sensor == CHILD_ID_DLY && message.type == V_VAR2) {
+    dly=message.getUInt();
+  }
 }
 
 void setup() { 
@@ -56,7 +78,10 @@ void setup() {
   gw.sendSketchInfo("Led driver", "2.1");
   gw.present(CHILD_ID_LED, S_DIMMER);
   gw.present(CHILD_ID_433, S_CUSTOM);
+  gw.present(CHILD_ID_FLS, S_LIGHT);
+  gw.present(CHILD_ID_DLY, S_CUSTOM);
   gw.request(CHILD_ID_LED, V_DIMMER);
+  gw.request(CHILD_ID_DLY, V_VAR2);
 }
 
 void loop() {
@@ -64,5 +89,8 @@ void loop() {
   if (mySwitch.available()) {
     gw.send(msg433.set(mySwitch.getReceivedValue()));
     mySwitch.resetAvailable();
+  }
+  if (fls > 0) {
+    ledFlash();
   }
 }
